e21cb795edbd281015d2a2b0651f86cc59e05d7b
Kauan <kauansantosdesouza45@gmail.com>
2023-09-19 14:11:29 -0300

    Colocando as categorias criado na aba models.py no admin.py e inserido a concatenação na urls.py

--- recipes/models.py
+++ recipes/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
+    def __str__(self):
+        return self.name
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -24,5 +26,7 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+    def __str__(self):
+        return self.title
     
     
