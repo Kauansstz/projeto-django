df2c776265a844bd98b362fb3e127abd73cced47
Kauan <kauansantosdesouza45@gmail.com>
2023-09-21 20:00:48 -0300

    Alterado o preparetions h1-h6 na aba css, arrumado a linguagem e a hora do Django e alterado a configuração a função Recipe na aba da view

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
-from django.http import Http404
+
 
 def home(request):
     recipes = Recipe.objects.filter( is_published = True).order_by('-id')
@@ -12,17 +12,15 @@ def home(request):
     
 def category(request, id):
     recipes = get_list_or_404(Recipe.objects.filter(category__id= id, is_published = True ).order_by('-id'))
-    # category_name = getattr(recipes.first(), 'category', None)
-    # if not recipes:
-    #     raise Http404('Página inexistente')
     return render(request, 'recipes/pages/category.html', context={
             'recipes': recipes,
-            'title': f'{recipes[0].category.name} - Category | '
+            'title': f'{recipes[0].category} - Category | '
         })
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(pk= id, is_published = True ).order_by('-id').first()
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
 
