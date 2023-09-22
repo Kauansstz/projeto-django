365c81a6617c7a92d8a9664a7dfcc9e36e7c135a
Kauan <kauansantosdesouza45@gmail.com>
2023-09-22 16:12:07 -0300

    Realizado outros testes e instalado o pytest-watch para facilitar os futuros testes

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,7 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class RecipeURLsTest(TestCase):
     def test_the_pytest_is_ok(self):
-        print("Hello, work")
-        assert 1 == 1
+        home_url = reverse("recipes-home")
+        self.assertEqual(home_url, "/")
+
+    def test_recipe_url_is_correct(self):
+        url = reverse("recipes-category", kwargs={"id": 1})
+        self.assertEqual(url, "/recipes/category/1/")
+
+    def test_recipe_detail_url_is_correct(self):
+        url = reverse("recipes-recipe", kwargs={"id": 1})
+        self.assertEqual(url, "/recipes/recipe/1/")
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,12 +1,9 @@
-
-
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    path('', views.home, name="recipes-home"),
-    path('recipes/<int:id>/', views.recipe, name='recipes-recipe'),
-    path('recipes/category/<int:id>/', views.category, name='recipes-category'),
+    path("", views.home, name="recipes-home"),
+    path("recipes/<int:id>/", views.recipe, name="recipes-recipe"),
+    path("recipes/category/<int:id>/", views.category, name="recipes-category"),
 ]
- 
