e34f7bdbe93b928aca4202c1b76824e72ce6e29f
Kauan <kauansantosdesouza45@gmail.com>
2023-09-28 16:03:51 -0300

    Adicionado aba do search e começado a configurar o mesmo, inserido unique na aba models na variavel slug

--- recipes/models.py
+++ recipes/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Category(models.Model):
     name = models.CharField(max_length=65)
-    
+
     def __str__(self):
         return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     decription = models.CharField(max_length=165)
-    slug = models.SlugField()
-    preparation_time= models.IntegerField()
+    slug = models.SlugField(unique=True)
+    preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
@@ -20,15 +22,20 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='media/recipes/covers/%Y/%m/%d/')
+    cover = models.ImageField(upload_to="media/recipes/covers/%Y/%m/%d/")
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None,
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        default=None,
     )
     author = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True, blank= True, default=None
+        User, on_delete=models.SET_NULL, null=True, blank=True, default=None
     )
+
     def __str__(self):
         return self.title
 
+
 # if __name__ == '__main__':
-    
--- recipes/views.py
+++ recipes/views.py
@@ -1,28 +1,40 @@
 from django.shortcuts import render, get_list_or_404
-from utils.recipes.factory import make_recipe
 from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter( is_published = True).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
-        
-    })
-    
+    recipes = Recipe.objects.filter(is_published=True).order_by("-id")
+    return render(
+        request,
+        "recipes/pages/home.html",
+        context={
+            "recipes": recipes,
+        },
+    )
+
+
 def category(request, id):
-    recipes = get_list_or_404(Recipe.objects.filter(category__id= id, is_published = True ).order_by('-id'))
-    return render(request, 'recipes/pages/category.html', context={
-            'recipes': recipes,
-            'title': f'{recipes[0].category} - Category | '
-        })
+    recipes = get_list_or_404(
+        Recipe.objects.filter(category__id=id, is_published=True).order_by("-id")
+    )
+    return render(
+        request,
+        "recipes/pages/category.html",
+        context={"recipes": recipes, "title": f"{recipes[0].category} - Category | "},
+    )
+
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(pk= id, is_published = True ).order_by('-id').first()
-    return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': recipe,
-        'is_detail_page': True,
-    })
+    recipe = Recipe.objects.filter(pk=id, is_published=True).order_by("-id").first()
+    return render(
+        request,
+        "recipes/pages/recipe-view.html",
+        context={
+            "recipe": recipe,
+            "is_detail_page": True,
+        },
+    )
 
 
-    
+def search(request):
+    return render(request, "recipes/pages/search.html")
