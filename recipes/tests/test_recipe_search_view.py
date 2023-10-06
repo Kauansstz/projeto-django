50387e4cff4bab2cda47a5e836083e7857de75d6
Kauan <kauansantosdesouza45@gmail.com>
2023-10-06 13:36:26 -0300

    Realizando teste de paginação, retirado  o script factory na pasta utils.

--- recipes/tests/test_recipe_search_view.py
+++ recipes/tests/test_recipe_search_view.py
@@ -1,18 +1,29 @@
-from django.test import TestCase
-from django.urls import reverse
+from django.urls import resolve, reverse
+from recipes import views
+
 from .test_recipe_base import RecipeTestBase
 
 
-class RecipeSearchTest(RecipeTestBase):
-    def test_recipe_search_raises_404_if_no_search_term(self):
-        url = reverse("recipes-search")
-        response = self.client.get(url)
-        self.assertAlmostEqual(response.status_code, 404)
+class RecipeSearchViewTest(RecipeTestBase):
+    def test_recipe_search_uses_correct_view_function(self):
+        resolved = resolve(reverse("recipes:search"))
+        self.assertIs(resolved.func, views.search)
 
-    def test_recipe_search_term_is_on_page_title_and_escape(self):
-        url = reverse("recipes-search") + "?q=Teste"
+    def test_recipe_search_loads_correct_template(self):
+        response = self.client.get(reverse("recipes:search") + "?q=teste")
+        self.assertTemplateUsed(response, "recipes/pages/search.html")
+
+    def test_recipe_search_raises_404_if_no_search_term(self):
+        url = reverse("recipes:search")
         response = self.client.get(url)
-        self.assertIn("Search for &quot;Teste&quot;", response.content.decode("utf-8"))
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_search_term_is_on_page_title_and_escaped(self):
+        url = reverse("recipes:search") + "?q=<Teste>"
+        response = self.client.get(url)
+        self.assertIn(
+            "Search for &quot;&lt;Teste&gt;&quot;", response.content.decode("utf-8")
+        )
 
     def test_recipe_search_can_find_recipe_by_title(self):
         title1 = "This is recipe one"
