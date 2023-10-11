8a9a6d43329fc4d04997b1783031a5392c46363a
Kauan <kauansantosdesouza45@gmail.com>
2023-10-11 18:35:52 -0300

    Corrigido o problema de alguns testes e feito a migração do models novamente, assim ajudando na correção do problema

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class Category(models.Model):
@@ -11,7 +11,7 @@ class Category(models.Model):
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
-    decription = models.CharField(max_length=165)
+    description = models.CharField(max_length=165)
     slug = models.SlugField(unique=True)
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
@@ -20,9 +20,11 @@ class Recipe(models.Model):
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to="media/recipes/covers/%Y/%m/%d/")
+    cover = models.ImageField(
+        upload_to="recipes/covers/%Y/%m/%d/", blank=True, default=""
+    )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
@@ -30,12 +32,7 @@ class Recipe(models.Model):
         blank=True,
         default=None,
     )
-    author = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True, blank=True, default=None
-    )
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
-
-
-# if __name__ == '__main__':
--- recipes/tests/test_view_home.py
+++ recipes/tests/test_view_home.py
@@ -1,8 +1,8 @@
-from django.test import TestCase
 from django.urls import reverse
+from .test_recipe_base import RecipeTestBase
 
 
-class RecipeHomeTest(TestCase):
+class RecipeHomeTest(RecipeTestBase):
     def test_recipe_home_view_returns_status_code_200_ok(self):
         response = self.client.get(reverse("recipes-home"))
         self.assertEqual(response.status_code, 200)
@@ -17,3 +17,12 @@ class RecipeHomeTest(TestCase):
             "<h1 class='response'>No Recipes Found Here</h1>",
             response.content.decode("utf-8"),
         )
+
+    def test_recipe_home_is_paginated(self):
+        for i in range(18):
+            kwargs = {"author_data": {"username": f"u{i}"}, "slug": f"r{i}"}
+            self.make_recipe(**kwargs)
+        response = self.client.get(reverse("recipes-home"))
+        recipes = response.context["recipes"]
+        paginatior = recipes.paginator
+        self.assertEqual(paginatior.num_pages, 2)
