e17b04cbe318bbfedee24d439db74f94e9f7e8d0
Kauan <kauansantosdesouza45@gmail.com>
2023-10-14 20:40:44 -0300

    Configurando a criação de usuário e visualizado como se comportar no network no navegador

--- authors/urls.py
+++ authors/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import register_view
+from .views import register_view, register_create
+
+app_name = "authors"
 
 urlpatterns = [
     path("register/", register_view, name="register"),  # type: ignore
+    path("register/create/", register_create, name="create"),  # type: ignore
 ]
