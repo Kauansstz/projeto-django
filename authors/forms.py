2d8c775dd06d7b70c3c06cca480caa37fdc076fd
Kauan <kauansantosdesouza45@gmail.com>
2023-10-20 15:26:10 -0300

    Alterado o forms, alterando o Nome da fields e acrescentando o placeholder nos fields

--- authors/forms.py
+++ authors/forms.py
@@ -3,6 +3,16 @@ from django.contrib.auth.models import User
 
 
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].widget.attrs["placeholder"] = "que legal"
+
+    confirm_password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password..."}),
+        label="Confirm Password",
+    )
+
     class Meta:
         model = User
         fields = [
@@ -12,3 +22,17 @@ class RegisterForm(forms.ModelForm):
             "email",
             "password",
         ]
+        labels = {
+            "username": "Username",
+            "first_name": "First Name",
+            "last_name": "Last Name",
+            "email": "E-mail",
+            "password": "Password",
+        }
+        widgets = {
+            "first_name": forms.TextInput(attrs={"placeholder": "First Name..."}),
+            "last_name": forms.TextInput(attrs={"placeholder": "Last Name..."}),
+            "username": forms.TextInput(attrs={"placeholder": "Username..."}),
+            "email": forms.TextInput(attrs={"placeholder": "E-mail..."}),
+            "password": forms.PasswordInput(attrs={"placeholder": "Password..."}),
+        }
