5209db057931ec62721740663651ec5eef6fd8c8
Kauan <kauansantosdesouza45@gmail.com>
2023-11-15 16:04:54 -0300

    Realizado algumas configurações no script browser

--- utils/browser.py
+++ utils/browser.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from time import sleep
 from selenium.webdriver.chrome.service import Service
 from pathlib import Path
 
@@ -6,10 +7,21 @@ ROOT_PATH = Path(__file__).parent.parent
 CHROMEDRIVER_NAME = "chromedriver.exe"
 CHROMEDRIVER_PATH = ROOT_PATH / "bin" / CHROMEDRIVER_NAME
 
-chrome_options = webdriver.ChromeOptions()
-chrome_service = Service(executable_path=CHROMEDRIVER_PATH)  # type: ignore
-browser = webdriver.Chrome(service=chrome_service, options=chrome_options)  # type: ignore
 
-browser.get("https://www.google.com.br")
+def make_chrome_browser(*options):
+    chrome_options = webdriver.ChromeOptions()
 
-# chrome_options.add_argument()
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)
+
+    chrome_service = Service(executable_path=CHROMEDRIVER_PATH)  # type: ignore
+    browser = webdriver.Chrome(service=chrome_service, options=chrome_options)  # type: ignore
+    return browser
+
+
+if __name__ == "__main__":
+    browser = make_chrome_browser("--headless")
+    browser.get("http://www.udemy.com/")
+    sleep(5)
+    browser.quit()
