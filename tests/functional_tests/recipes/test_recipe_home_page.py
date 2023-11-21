37f800864d7605e142b00ac773244e00b86ce852
Kauan <kauansantosdesouza45@gmail.com>
2023-11-21 16:07:39 -0300

    Inserido functional_test em pytest.ini e usado em test_recipe

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,10 +1,12 @@
-from base import RecipeBaseFunctionalTest
+from .base import RecipeBaseFunctionalTest
 from selenium.webdriver.common.by import By
+import pytest
 
 
+@pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
     def test_recipe_home_page_without_recipes_not_found_message(self):
         self.browser.get(self.live_server_url)
-        self.sleep(6)
+        self.sleep(2)
         body = self.browser.find_element(By.TAG_NAME, "body")
         self.assertIn("No Recipes Found Here", body.text)
