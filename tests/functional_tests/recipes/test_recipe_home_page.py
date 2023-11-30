6802fcfac7c31a12cb5acce5684a453dbb21a463
Kauan <kauansantosdesouza45@gmail.com>
2023-11-30 16:41:40 -0300

    è isso

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -6,7 +6,8 @@ import pytest
 @pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
     def test_recipe_home_page_without_recipes_not_found_message(self):
+        self.make_recipe_in_batch(qtd=20)
         self.browser.get(self.live_server_url)
-        self.sleep(2)
         body = self.browser.find_element(By.TAG_NAME, "body")
-        self.assertIn("No Recipes Found Here", body.text)
+        self.sleep()
+        self.assertIn("", body.text)
