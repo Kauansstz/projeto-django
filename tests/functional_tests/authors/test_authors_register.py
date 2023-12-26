75f08a75b7e2b0c0c0efac8cd86a2381caa4d745
Kauan <kauansantosdesouza45@gmail.com>
2023-12-25 21:36:39 -0300

    Realizado testes porem está com problema no XPATH

--- tests/functional_tests/authors/test_authors_register.py
+++ tests/functional_tests/authors/test_authors_register.py
@@ -1,7 +1,22 @@
 from .base import AuthorsBaseTest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class AuthorsRegisterTest(AuthorsBaseTest):
-    def test_the_test(self):
-        self.browser.get(self.live_server_url + "/authors/register/")
-        self.sleep()
+    # def get_by_placeholder(self, web_element, placeholder):
+    #     return web_element.find_element(
+    #         By.XPATH, f'//input[@placeholder="{placeholder}"]'
+    #     )
+
+    # def test_the_test(self):
+    #     self.browser.get(self.live_server_url + "/authors/register/")
+    #     form = self.browser.find_element(
+    #         By.XPATH,
+    #         "/html/body/main/div[2]/form",
+    #     )
+    #     first_name_fiel = self.get_by_placeholder(form, "EX.:John")
+    #     first_name_field.send_keys("  ")
+    #     first_name_field.send_keys(Keys.ENTER)
+    # self.sleep(5)
+    ...
