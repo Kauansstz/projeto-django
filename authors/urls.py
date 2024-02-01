cd74bdc551f3db1c8bcedd6f256a28c774d404ad
Kauan <kauansantosdesouza45@gmail.com>
2024-02-01 14:28:09 -0300

    Create deleted class

--- authors/urls.py
+++ authors/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     ),
     path(
         "dashboard/recipe/delete/",
-        views.dashboad_recipe_delete,
+        views.DashboardRecipeDelete.as_view(),
         name="dashboard_recipe_delete",
     ),
     path(
