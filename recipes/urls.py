e2a4a22e8b9baf34235a4c84ac969c870233483b
Kauan <kauansantosdesouza45@gmail.com>
2024-02-02 13:47:43 -0300

    Create skills file in main folder, inserted skills section in index

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path("", views.RecipeListViewHome.as_view(), name="recipes-home"),
     path("recipes/search/", views.search, name="recipes-search"),
     path("recipes/<int:id>/", views.recipe, name="recipes-recipe"),
-    path("recipes/category/<int:id>/", views.category, name="recipes-category"),
+    path(
+        "recipes/category/<int:id>/",
+        views.RecipeListViewCategory.as_view(),
+        name="recipes-category",
+    ),
 ]
