53ceaa0c8158b8d4ecb1488c2f3a893fb41fba2c
Kauan <kauansantosdesouza45@gmail.com>
2024-02-02 13:53:11 -0300

    Create RecipeListViewSearch class in recipe

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.RecipeListViewHome.as_view(), name="recipes-home"),
-    path("recipes/search/", views.search, name="recipes-search"),
+    path(
+        "recipes/search/", views.RecipeListViewSearch.as_view(), name="recipes-search"
+    ),
     path("recipes/<int:id>/", views.recipe, name="recipes-recipe"),
     path(
         "recipes/category/<int:id>/",
